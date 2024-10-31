63f7ff9cfc5121bd66f24abe707a7515dca83390
Daniil Chernousov <i@tehnik-7.ru>
2024-10-31 05:50:50 +0300

    add greetings

--- index.js
+++ index.js
@@ -1,5 +1,30 @@
 const TelegramBot = require('node-telegram-bot-api')
 const express = require('express')
-const cors = require('cors')
 require('dotenv/config.js')
 
+const webAppUrl = process.env.WEB_APP_URL
+const greeting = `🎉 Добро пожаловать в интернет-магазин электронной техники! Здесь вы найдете товары по самым лучшим ценам!`
+
+const bot = new TelegramBot(process.env.TG_BOT_TOKEN, {polling: true}) 
+const app = express()
+
+app.use(express.json())
+
+bot.on('message', async (msg) => {
+  const chatId = msg.chat.id
+  const text = msg.text
+
+  if (text == '/start') {
+    console.log(`Новый пользователь: Имя: ${msg.chat.first_name}, Ник: ${msg.chat.username}, Айди: ${msg.chat.id} `)
+    await bot.sendMessage(chatId, greeting, {
+      reply_markup: {
+        resize_keyboard: true,
+        keyboard: [
+          [{text: "Сделать заказ", web_app: {url: webAppUrl}}]
+        ]
+      }
+    })
+  }
+})
+
+app.listen(process.env.PORT, () => console.log("Server has been started"))
